9267e7c253e6813f5e07871a5acacde305720a5e
Rob Howard <rob@robhoward.id.au>
2017-02-06 09:02:17 +1100

    Adds timezone to "meetup time in local time" display.

--- static/js/gathering.js
+++ static/js/gathering.js
@@ -4,7 +4,8 @@ var convertDates = function() {
     var datetimes = document.getElementsByClassName('datetime');
     for (var i = 0; i < datetimes.length; i++) {
         var dt = datetimes[i];
-        dt.innerHTML = moment.utc(dt.innerHTML.slice(0, -6), 'YYYY-MM-DD HH:mm').local().format("dddd, MMMM Do YYYY, h:mm a");
+        var original = dt.innerHTML.slice(0, -6);
+        dt.innerHTML = moment.utc(original, 'YYYY-MM-DD HH:mm').local().format("dddd, MMMM Do YYYY, h:mm a (UTCZZ)");
     }
 };
 
