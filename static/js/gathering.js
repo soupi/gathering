282c3f1911be4e2ecb2c84ce768e4a36e535b0ed
Rob Howard <rob@robhoward.id.au>
2017-02-07 09:23:09 +1100

    Quick reformat of moment tz code.

--- static/js/gathering.js
+++ static/js/gathering.js
@@ -4,8 +4,10 @@ var convertDates = function() {
     var datetimes = document.getElementsByClassName('datetime');
     for (var i = 0; i < datetimes.length; i++) {
         var dt = datetimes[i];
-        var original = dt.innerHTML.slice(0, -6);
-        dt.innerHTML = moment.utc(original, 'YYYY-MM-DD HH:mm').local().format("dddd, MMMM Do YYYY, h:mm a (UTCZZ)");
+        dt.innerHTML =
+          moment.utc(dt.innerHTML.slice(0, -6), 'YYYY-MM-DD HH:mm')
+            .local()
+            .format("dddd, MMMM Do YYYY, h:mm a (UTCZZ)");
     }
 };
 
